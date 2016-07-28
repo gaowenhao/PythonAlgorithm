b9e904830a7d290b4db829f6bb89cef10fdd1fbc
Vincent <vevz@163.com>
2016-07-28 15:25:39 +0800

    简单的加密算法

---
+++ encryption/caret_encryption.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import array
+
+SECURITY_KEY = 1995
+
+
+# 采用简单的异或实现加密算法，根据定理：A ^ B ^ B = A
+def encode(key):
+    char_array = array.array('B', key)
+    result = []
+    for char in char_array:
+        result.append(char ^ SECURITY_KEY)
+    return result
+
+
+def decode(security_message):
+    result = []
+    for char in security_message:
+        result.append(chr(char ^ SECURITY_KEY))
+    return "".join(result)
+
+
+if __name__ == "__main__":
+    temp_key = "iloveu"
+    temp_security_message = encode(temp_key)
+    print(temp_security_message)
+    print(decode(temp_security_message))
