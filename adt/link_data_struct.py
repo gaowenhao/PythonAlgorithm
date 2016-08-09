90905668fce83ac53d2fbe2188f3c4ae685035e8
Vincent <vevz@163.com>
2016-08-09 17:35:06 +0800

    链表数据结构

--- adt/link_data_struct.py
+++ adt/link_data_struct.py
@@ -35,20 +35,33 @@ class LinkList:
         self._insert_to_last(data=data)
         return True
 
+    # 同上,链表结构默认就是插入到最后一个位置
+    def insert_last(self, data):
+        self._insert_to_last(data=data)
+        return True
+
+    # 插入到第一个位置
+    def insert_first(self, data):
+        self._insert_to_first(data=data)
+        return True
+
+    # 实现这个方法
+    def insert_all(self, data, index):
+        self._check_position(index)
+        current = self.last
+        for value in data:
+            pass
+
     # 插入到某个角标的位置
     def insert_to_index(self, data, index):
-        if self._check_position(index):
-            self._insert_before(data, index)
-            return True
-        else:
-            return False
+        self._check_position(index)
+        self._insert_before(data, index)
+        return True
 
     # 检查某个角标是否合理
     def _check_position(self, index):
         if index > self.size or index < 1:
-            return False
-        else:
-            return True
+            raise Exception
 
     # 寻找某个角标的值
     def _node(self, index):
@@ -66,13 +79,24 @@ class LinkList:
     # 插入到最后的实际执行方法
     def _insert_to_last(self, data):
         l = self.last  # 先获取最后一个元素
-        new_node = Node(data, l,None)  # 初始化一个节点，并设置其prev属性为最后
-        self.last = new_node  # 把最后元素设置为新节点
+        new_node = Node(data, l, None)
+        self.last = new_node
 
         if l is None:  # 如果最后节点为空，说明链表为空
             self.first = new_node
         else:
             l.next = new_node
+
+        self.size += 1
+
+    def _insert_to_first(self, data):
+        f = self.first
+        new_node = Node(data, None, f)
+        self.first = new_node
+        if f is None:
+            self.last = new_node
+        else:
+            f.prev = new_node
 
         self.size += 1
 
@@ -92,11 +116,11 @@ class LinkList:
 
 if __name__ == "__main__":
     link_list = LinkList()
-    link_list.insert(12)
+    link_list.insert_first(12)
     link_list.insert(13)
 
     link_list.insert(14)
-    link_list.insert(15)
+    link_list.insert_first(15)
     link_list.show()
 
     print(link_list.insert_to_index(11111, 1))
